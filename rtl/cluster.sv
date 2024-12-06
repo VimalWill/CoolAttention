deb7d69e92496fe5035224566dbed371f3bb9aae
VimalWill <vimalwilliam99@gmail.com>
2024-12-06 19:57:12 +0530

    updated mac

--- rtl/cluster.sv
+++ rtl/cluster.sv
@@ -14,35 +14,17 @@ module mac_cluster #(
     input logic [DATA_WIDTH-1:0] m2_b,
 ); 
 
-    logic [PROD_WIDTH-1:0] m1_res, m2_res; 
-    logic [PROD_WIDTH-1:0] res_12; 
-    logic crry_12; 
+    logic [PROD_WIDTH-1:0] res_12, crry_12; 
 
-    multiplier_8bit mul_1(
+    mac_8bit mac_1(
         .clk(clk), 
         .rst(rst), 
         .ebl(ebl), 
         .a_in(m1_a), 
         .b_in(m1_b), 
-        .out(m1_res)
-    ); 
-
-       multiplier_8bit mul_1(
-        .clk(clk), 
-        .rst(rst), 
-        .ebl(ebl), 
-        .a_in(m2_a), 
-        .b_in(m2_b), 
-        .out(m2_res)
-    );
-
-    adder_16bit add_1(
-        .clk(clk),
-        .rst(rst), 
-        .ebl(ebl), 
-        .a_in(m1_res),
-        .b_in(m2_res), 
-        .out(res_12); 
+        .c_in(m2_a), 
+        .d_in(m2_b), 
+        .out(res_12), 
         .carry_out(crry_12)
     ); 
 
