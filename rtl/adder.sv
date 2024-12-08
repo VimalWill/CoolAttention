c15fca57a11a62c4e33b549140bcda6240ebe172
VimalWill <vimalwilliam99@gmail.com>
2024-12-08 17:05:18 +0530

    should work on the 16-bit adder

--- rtl/adder.sv
+++ rtl/adder.sv
@@ -1,99 +1,94 @@
+// ref: https://www.youtube.com/watch?v=KT7mukuxKCE
 module half_adder(
-    input logic clk, 
-    input logic rst, 
-    input logic ebl, 
-    input logic a_in, 
-    input logic b_in, 
-
-    output logic sum, 
+    input logic clk,
+    input logic rst,
+    input logic ebl,
+    input logic a_in,
+    input logic b_in,
+    output logic sum,
     output logic carry
 );
-    logic itl_sum; 
-    logic itl_carry; 
+    logic itl_sum;
+    logic itl_carry;
 
     always_ff @(posedge clk or posedge rst) begin
         if (rst) begin
-            itl_sum <= 1'b0; 
-            itl_carry <= 1'b0; 
+            itl_sum <= 1'b0;
+            itl_carry <= 1'b0;
         end else if (ebl) begin
-            itl_sum <= a_in ^ b_in; 
-            itl_carry <= a_in && b_in; 
+            itl_sum <= a_in ^ b_in;
+            itl_carry <= a_in && b_in;
         end
     end
 
-    assign sum = itl_sum; 
-    assign carry = itl_carry; 
-
+    assign sum = itl_sum;
+    assign carry = itl_carry;
 endmodule
 
 module full_adder(
-    input logic clk, 
-    input logic rst, 
-    input logic ebl, 
-    input logic a_in, 
-    input logic b_in, 
-    input logic crry_in, 
-
-    output logic sum, 
+    input logic clk,
+    input logic rst,
+    input logic ebl,
+    input logic a_in,
+    input logic b_in,
+    input logic carry_in,
+    output logic sum,
     output logic carry
-); 
-    logic ha1_sum, ha2_sum; 
-    logic ha1_crry, ha2_crry; 
+);
+    logic ha1_sum, ha2_sum;
+    logic ha1_carry, ha2_carry;
 
-   
     half_adder ha_1(
-        .clk(clk), 
-        .rst(rst), 
-        .ebl(ebl), 
-        .a_in(a_in), 
-        .b_in(b_in), 
-        .sum(ha1_sum), 
-        .carry(ha1_crry)
-    ); 
+        .clk(clk),
+        .rst(rst),
+        .ebl(ebl),
+        .a_in(a_in),
+        .b_in(b_in),
+        .sum(ha1_sum),
+        .carry(ha1_carry)
+    );
 
     half_adder ha_2(
-        .clk(clk), 
-        .rst(rst), 
-        .ebl(ebl), 
-        .a_in(ha1_sum), 
-        .b_in(crry_in), 
-        .sum(ha2_sum), 
-        .carry(ha2_crry)
-    ); 
+        .clk(clk),
+        .rst(rst),
+        .ebl(ebl),
+        .a_in(ha1_sum),
+        .b_in(carry_in),
+        .sum(ha2_sum),
+        .carry(ha2_carry)
+    );
 
     always_ff @(posedge clk or posedge rst) begin
-        if (rst) begin 
-            sum <= 1'b0; 
-            carry <= 1'b0; 
+        if (rst) begin
+            sum <= 1'b0;
+            carry <= 1'b0;
         end else if (ebl) begin
-            sum <= ha2_sum; 
-            carry <= ha1_crry || ha2_crry; 
+            sum <= ha2_sum;
+            carry <= ha1_carry || ha2_carry;
         end
     end
-
 endmodule
 
 module adder_16bit #(
     parameter DATA_WIDTH = 16
 )(
-    input logic clk, 
-    input logic rst, 
+    input logic clk,
+    input logic rst,
     input logic ebl,
-    input logic [DATA_WIDTH-1:0] a_in, 
-    input logic [DATA_WIDTH-1:0] b_in, 
+    input logic [DATA_WIDTH-1:0] a_in,
+    input logic [DATA_WIDTH-1:0] b_in,
     output logic [DATA_WIDTH-1:0] out,
     output logic carry_out
 );
-
     logic [DATA_WIDTH-1:0] carry;
 
     half_adder ha_1(
-        .clk(clk), 
-        .rst(rst), 
-        .ebl(ebl), 
-        .a_in(a_in[0]), 
-        .b_in(b_in[0]), 
-        .sum(out[0]), 
+        .clk(clk),
+        .rst(rst),
+        .ebl(ebl),
+        .a_in(a_in[0]),
+        .b_in(b_in[0]),
+        .sum(out[0]),
         .carry(carry[0])
     );
 
@@ -101,18 +96,17 @@ module adder_16bit #(
     generate
         for (i = 1; i < DATA_WIDTH; i++) begin : full_adders
             full_adder fa_i(
-                .clk(clk), 
-                .rst(rst), 
-                .ebl(ebl), 
-                .a_in(a_in[i]), 
-                .b_in(b_in[i]), 
-                .crry_in(carry[i-1]), 
-                .sum(out[i]), 
+                .clk(clk),
+                .rst(rst),
+                .ebl(ebl),
+                .a_in(a_in[i]),
+                .b_in(b_in[i]),
+                .carry_in(carry[i-1]),
+                .sum(out[i]),
                 .carry(carry[i])
             );
         end
     endgenerate
 
     assign carry_out = carry[DATA_WIDTH-1];
-
 endmodule
--- rtl/mac.sv
+++ rtl/mac.sv
@@ -1,60 +1,58 @@
 module mac_8bit #(
-    parameter DATA_WIDTH = 8, 
-    parameter PROD_WIDTH = 16, 
+    parameter DATA_WIDTH = 8,
+    parameter PROD_WIDTH = 16
 )(
-    input logic clk, 
-    input logic rst, 
-    input logic ebl, 
-    input logic [DATA_WIDTH-1:0] a_in, 
-    input logic [DATA_WIDTH-1:0] b_in, 
-    input logic [DATA_WIDTH-1:0] c_in, 
-    input logic [DATA_WIDTH-1:0] d_in, 
-    output logic [PROD_WIDTH-1:0] out, 
-    output logic [PROD_WIDTH-1:0] carry_out
-); 
+    input logic clk,
+    input logic rst,
+    input logic ebl,
+    input logic [DATA_WIDTH-1:0] a_in,
+    input logic [DATA_WIDTH-1:0] b_in,
+    input logic [DATA_WIDTH-1:0] c_in,
+    input logic [DATA_WIDTH-1:0] d_in,
+    output logic [PROD_WIDTH-1:0] out,
+    output logic carry_out
+);
 
-    logic [PROD_WIDTH-1:0] mul1_res, mul2_res; 
-    logic [PROD_WIDTH-1:0] tmp_sum, tmp_crry; 
-
-    multiplier_8bit mul_1(
-        .clk(clk), 
-        .rst(rst), 
-        .ebl(ebl), 
-        .a_in(a_in), 
-        .b_in(b_in), 
-        .out(mul1_res)
-    ); 
-
-    multiplier_8bit mul_2(
-        .clk(clk), 
-        .rst(rst), 
-        .ebl(ebl), 
-        .a_in(c_in), 
-        .b_in(d_in), 
-        .out(mul2_res)
-    ); 
-
-    adder_16bit adder(
-        .clk(clk), 
-        .rst(rst), 
-        .a_in(mul1_res), 
-        .b_in(mul2_res), 
-        .sum(tmp_sum), 
-        .carry_out(tmp_crry)
-    ); 
+    logic [PROD_WIDTH-1:0] mul1_res, mul2_res;
+    logic [PROD_WIDTH-1:0] sum_res;
+    logic adder_carry_out;
 
     always_ff @(posedge clk or posedge rst) begin
         if (rst) begin
-            mul1_res <= 16'b0; 
-            mul2_res <= 16'b0; 
-            tmp_sum <= 16'b0; 
-            tmp_crry <= 16'b0; 
-        end 
+            out <= '0;
+            carry_out <= 1'b0;
+        end else if (ebl) begin
+            out <= sum_res;
+            carry_out <= adder_carry_out;
+        end
+    end
 
-    assign out = tmp_sum; 
-    assign carry_out = tmp_crry; 
-    end 
+    multiplier_8bit mul_1(
+        .clk(clk),
+        .rst(rst),
+        .ebl(ebl),
+        .a_in(a_in),
+        .b_in(b_in),
+        .out(mul1_res)
+    );
 
-    
+    multiplier_8bit mul_2(
+        .clk(clk),
+        .rst(rst),
+        .ebl(ebl),
+        .a_in(c_in),
+        .b_in(d_in),
+        .out(mul2_res)
+    );
+
+    adder_16bit adder(
+        .clk(clk),
+        .rst(rst),
+        .ebl(ebl),
+        .a_in(mul1_res),
+        .b_in(mul2_res),
+        .out(sum_res),
+        .carry_out(adder_carry_out)
+    );
 
 endmodule
